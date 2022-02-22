1dd278b5354df297a59691602963477669914260
Osterhammer <l1222@schueler.ms-muc.de>
2022-02-22 09:52:32 +0100

    Stromstoßschalter

--- TestTimeDelay/main/examples/EXAMPLE_RS/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_RS/main.cpp
@@ -21,8 +21,9 @@ static const char * const TAG = "EXAMPLE_RS";
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
 #define BUTTON_I2 GPIO_NUM_32        // Pin 32.
+#define BUTTON_I3 GPIO_NUM_39
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
-
+#define GPIO_Q2 GPIO_NUM_23
 
 
 /* Inside .cpp file, app_main function must be declared with C linkage */
@@ -39,30 +40,38 @@ extern "C" void app_main(void)
        functions.)
     */
     gpio_reset_pin(GPIO_Q1);
+    gpio_reset_pin(GPIO_Q2);
     gpio_reset_pin(BUTTON_I1);
     gpio_reset_pin(BUTTON_I2);
+    gpio_reset_pin(BUTTON_I3);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
     gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
-    gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
+    gpio_set_direction(BUTTON_I3, GPIO_MODE_INPUT);
+ //set to 0 at Reset.
 
 
     RS RS1;
+    RS RS2;
 
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
         bool I2 = not gpio_get_level(BUTTON_I2);
+        bool I3 = not gpio_get_level(BUTTON_I3);
 
         //RS1(SET, RESET1);
-        RS1(I1, I2);
+        RS1(I3, I2);
+        RS2(I3, I1);
 
 
 
         // Ausgaenge setzen
         gpio_set_level(GPIO_Q1, RS1.Q1);
+        gpio_set_level(GPIO_Q2, RS2.Q2);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
