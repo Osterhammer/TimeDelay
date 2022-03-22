61ffc3c7b0d40e949e66d69e9f137d0aba0fafea
Osterhammer <l1222@schueler.ms-muc.de>
2022-03-22 08:09:17 +0100

    Test Spiegel

--- TestTimeDelay/main/examples/EXAMPLE_TOF/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TOF/main.cpp
@@ -21,6 +21,7 @@ static const char * const TAG = "EXAMPLE_TON";
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
 #define BUTTON_I2 GPIO_NUM_32
+#define BUTTON_I3 GPIO_NUM_39
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
 #define GPIO_Q2 GPIO_NUM_23
 
@@ -42,11 +43,13 @@ extern "C" void app_main(void)
     gpio_reset_pin(GPIO_Q2);
     gpio_reset_pin(BUTTON_I1);
     gpio_reset_pin(BUTTON_I2);
+    gpio_reset_pin(BUTTON_I3);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
     gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I3, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
     gpio_set_level(GPIO_Q2, 0); //set to 0 at Reset.
 
@@ -57,20 +60,29 @@ extern "C" void app_main(void)
     TP TP2;
     TP2.PT = 7500;
 
+    SR SR1;
+    SR SR2;
+
 
     while (true) // Endlos-Schleife
     {
         // Eingang lesen, das not wird gebraucht weil die Eingaenge bei losgelassenem Taster auf 3.3V sind, und der Taster auf GND schaltet.
         bool I1 = not gpio_get_level(BUTTON_I1);
         bool I2 = not gpio_get_level(BUTTON_I2);
+        bool I3 = not gpio_get_level(BUTTON_I3);
 
         // den I1 an TOF2 uebergeben, und TOF2 aufrufen
+
+
         TP1(I1);
         TP2(I2);
 
+        SR1 (TP1.Q, I3);
+        SR2 (TP2.Q, I3);
+
         // Ausgaenge setzen
-        gpio_set_level(GPIO_Q1, TP1.Q);
-        gpio_set_level(GPIO_Q2, TP2.Q);
+        gpio_set_level(GPIO_Q1, SR1.Q1);
+        gpio_set_level(GPIO_Q2, SR2.Q1);
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
